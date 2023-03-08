51a450762ce2755b1d2d9b702a926dee2e351466
Cleóbulo Oliveira <cleobulo.oliveira@itriad.org.br>
2023-03-08 15:37:51 -0400

    refactor: add some comments

--- src/modules/tasks/http/controllers/TasksController.ts
+++ src/modules/tasks/http/controllers/TasksController.ts
@@ -3,13 +3,24 @@ import { todoList } from '../../../../shared/data-source';
 
 export default class TasksController {
     public create(request: any) {
+        /**
+         * Define data to save
+         */
         const { title, description } = request.body;
         const insertTaskData = {
             id: todoList.length + 1,
             title: title,
             description: description
         }
+
+        /**
+         * Save data on static list
+         */
         todoList.push(insertTaskData);
+
+        /**
+         * Return operation status
+         */
         return created();
     }
 }
